2605233436f81241431d86bee4e54df418754fa3
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-07 15:51:01 -0400

    added second test

--- tests.py
+++ tests.py
@@ -17,6 +17,19 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
+    def test_maze_create_cells_2(self):
+        num_cols = 5
+        num_rows = 5
+        m1 = Maze(0, 0, num_rows, num_cols, 5, 5)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
