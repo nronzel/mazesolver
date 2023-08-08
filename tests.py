e70fb293681e8e05f54d59378d046ed1aeec9ad4
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-08 16:46:58 -0400

    removed redundant test

--- tests.py
+++ tests.py
@@ -17,19 +17,6 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
-    def test_maze_create_cells_2(self):
-        num_cols = 5
-        num_rows = 5
-        m1 = Maze(0, 0, num_rows, num_cols, 5, 5)
-        self.assertEqual(
-            len(m1._cells),
-            num_cols,
-        )
-        self.assertEqual(
-            len(m1._cells[0]),
-            num_rows,
-        )
-
     def test_break_entrance_and_exit(self):
         num_cols = 5
         num_rows = 5
