2773cc889c3de135900581ca214e0ed57d88e6a2
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-08 16:45:34 -0400

    add test for _reset_cells_visited
    also fixed tests for break_entrance_and_exit

--- tests.py
+++ tests.py
@@ -36,14 +36,28 @@ class Tests(unittest.TestCase):
         m1 = Maze(0, 0, num_rows, num_cols, 5, 5)
 
         self.assertEqual(m1._cells[0][0].has_top_wall, False)
-        self.assertEqual(m1._cells[0][0].has_left_wall, True)
-        self.assertEqual(m1._cells[0][0].has_right_wall, True)
-        self.assertEqual(m1._cells[0][0].has_bottom_wall, True)
-
         self.assertEqual(m1._cells[-1][-1].has_bottom_wall, False)
-        self.assertEqual(m1._cells[-1][-1].has_top_wall, True)
-        self.assertEqual(m1._cells[-1][-1].has_left_wall, True)
-        self.assertEqual(m1._cells[-1][-1].has_right_wall, True)
+
+    def test_reset_cells_visited(self):
+        num_cols = 5
+        num_rows = 5
+        m1 = Maze(0, 0, num_rows, num_cols, 5, 5)
+        # set each cell visited to True
+        for column in m1._cells:
+            for cell in column:
+                cell.visited = True
+        # assert visited = True
+        for column in m1._cells:
+            for cell in column:
+                self.assertEqual(cell.visited, True)
+
+        # reset visited to False
+        m1._reset_cells_visited()
+
+        # assert visited is False for each cell
+        for column in m1._cells:
+            for cell in column:
+                self.assertEqual(cell.visited, False)
 
 
 if __name__ == "__main__":
