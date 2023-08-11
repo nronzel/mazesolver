0a7a958d7844a7d9ca98c78039252506410ca644
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-11 13:03:45 -0400

    added comments
    added comments

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ from maze import Maze
 
 
 def main():
+    # set size of maze
     num_rows = 16
     num_cols = 16
+
+    # sets the offset of the maze from the sides of the Window, and
+    # the size of each cell. Smaller number = smaller cells
     margin = 25
+
     screen_x = 800
     screen_y = 600
+
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
     win = Window(screen_x, screen_y)
