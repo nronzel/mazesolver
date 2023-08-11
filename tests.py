cb4263f8c0637405b625f2b6c1194dc8508549a1
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-11 12:49:37 -0400

    updated test params

--- tests.py
+++ tests.py
@@ -18,17 +18,17 @@ class Tests(unittest.TestCase):
         )
 
     def test_break_entrance_and_exit(self):
-        num_cols = 5
-        num_rows = 5
-        m1 = Maze(0, 0, num_rows, num_cols, 5, 5)
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
 
         self.assertEqual(m1._cells[0][0].has_top_wall, False)
         self.assertEqual(m1._cells[-1][-1].has_bottom_wall, False)
 
     def test_reset_cells_visited(self):
-        num_cols = 5
-        num_rows = 5
-        m1 = Maze(0, 0, num_rows, num_cols, 5, 5)
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
 
         for column in m1._cells:
             for cell in column:
