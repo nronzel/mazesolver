79f3142922305a7601f246464289b5c5fac07a1e
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-11 12:50:04 -0400

    solve maze

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from maze import Maze
 
 
 def main():
-    num_rows = 12
+    num_rows = 16
     num_cols = 16
-    margin = 50
+    margin = 25
     screen_x = 800
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_cols
@@ -13,6 +13,7 @@ def main():
     win = Window(screen_x, screen_y)
 
     maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+    maze.solve()
 
     win.wait_for_close()
 
